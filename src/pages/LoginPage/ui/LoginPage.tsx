14ede6bbe5f1539f7759a5336e5e3f30335f0442
Kiprin Konstantin <kiprin.konstantin@gmail.com>
2023-10-19 21:15:26 +0300

    login page step 1

--- src/pages/LoginPage/ui/LoginPage.tsx
+++ src/pages/LoginPage/ui/LoginPage.tsx
@@ -1,5 +1,17 @@
 import { FC, ReactElement, memo } from "react";
+import "./style.css";
 
 export const LoginPage: FC = memo((): ReactElement => {
-  return <div>login page</div>;
+  return (
+    <div className="login-page">
+      <h1>Вход</h1>
+      <p className="login-page__subtitle">Уникальная технология доступна для вашего бизнеса уже сейчас!</p>
+
+      <form>
+        {/* input */}
+        {/* input */}
+        {/* button */}
+      </form>
+    </div>
+  );
 });
