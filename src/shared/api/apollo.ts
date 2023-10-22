2377407312bc2bd95f5a969962ddc153f19bde24
Kiprin Konstantin <kiprin.konstantin@gmail.com>
2023-10-23 01:38:17 +0300

    fix graphQL authorization

--- src/shared/api/apollo.ts
+++ src/shared/api/apollo.ts
@@ -1,7 +1,24 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import { baseUrl } from "shared/const/apiUrl";
+import { LOCAL_STORAGE_USER_KEY } from "shared/const/localStorage";
+
+const httpLink = createHttpLink({
+  uri: baseUrl,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(LOCAL_STORAGE_USER_KEY);
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 export const apolloClient = new ApolloClient({
-  uri: baseUrl,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
